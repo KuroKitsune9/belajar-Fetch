6e2a02228d974ce7f6e298d7d0617afce3b37001
MuharafiDalilah <rafidalilah@gmail.com>
2023-08-08 13:38:56 +0700

    belajar-Fetch

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "vuex";
+import axios from "axios";
+
+export default createStore({
+    state: {
+        products : []
+    },
+    getters: {
+        getProducts: (state) => state.products
+    },
+    actions: {
+        async fetchProduct({ commit }) {
+            try {
+                const dataProduct = await axios.get("https://fakestoreapi.com/products")
+                commit('SET_PRODUCTS', dataProduct.data)
+            } catch (error) {
+                alert ('Ada error')
+                console.log(error)
+            }
+        }
+    },
+    mutations: {
+        SET_PRODUCTS(state, products) {
+            state.products = products
+        }
+    }
+})
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Halaman Product</h1>
+        <div v-for="data in items" :key="data.id">
+            <img :src="data.image" alt="" style="height: 100px;">
+        <i>{{ data.title }}</i>
+        </div>
+    </div>
+</template>
+
+<script>
+// import { computed, onMounted } from 'vue';
+// import { useStore } from 'vuex';
+
+// const store = useStore();
+// const getProducts = computed(() =>{
+//     return store.getters.getProducts
+// })
+
+// const products = computed(()=> {
+//     return store.satte.products
+// })
+
+// onMounted(() => {
+//     store.dispatch('fetchProduct')
+// })
+
+export default {
+    computed: {
+        getProducts() {
+            return this.$store.getter.getProducts
+        },
+        items() {
+            return this.$store.state.products
+        }
+    },
+    mounted () {
+        this.$store.dispatch('fetchProduct')
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
